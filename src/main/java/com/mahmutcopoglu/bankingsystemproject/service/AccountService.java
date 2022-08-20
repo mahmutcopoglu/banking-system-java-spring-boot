dea7a0a8c4eb8dbc0671a7272455010b5844b103
mahmutcopoglu <mahmutcopoglu@gmail.com>
2022-08-20 03:54:09 +0300

    first commit

---
+++ src/main/java/com/mahmutcopoglu/bankingsystemproject/service/AccountService.java
@@ -0,0 +1,26 @@
+package com.mahmutcopoglu.bankingsystemproject.service;
+
+import com.mahmutcopoglu.bankingsystemproject.enums.AccountType;
+import com.mahmutcopoglu.bankingsystemproject.models.Account;
+
+import java.util.List;
+
+public interface AccountService {
+
+    Account save(int bankId, AccountType type);
+
+    boolean hasAuth(int id);
+
+    Account findById(int id);
+
+    boolean remove(int id);
+
+    Account addAmount(int id, double amount);
+
+    boolean transfer(double amount, int senderAccountId, int receiverAccountId);
+
+    boolean typeControl(AccountType type);
+
+    List<Account> getUserAccounts();
+
+}
